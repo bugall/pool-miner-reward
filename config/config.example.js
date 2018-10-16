7e1e3814acf6a5ed69ce9be5377b071641364316
bugall <769088641@qq.com>
2018-10-16 16:18:58 +0800

    add example config

---
+++ config/config.example.js
@@ -0,0 +1,39 @@
+module.exports = {
+    'address': 'AFoXi9fqxPxoKSiAQhQRUyMZjuKp9GzP1k',
+    'rewardRecipients': {
+        'AFoXi9fqxPxoKSiAQhQRUyMZjuKp9GzP1k': 1.5,
+        'AFoh2BoTjtrNS6jvLFZfw464bhVnpeeXqa': 0.1
+    },
+    'paymentProcessing': {
+        'enabled': true,
+        'paymentInterval': 10,
+        'minimumPayment': 0.01,
+        'daemon': {
+            'host': '',
+            'port': '',
+            'user': '',
+            'password': ''
+        }
+    },
+    coin: {
+        name: '',
+        symbol: '',
+        algorithm: ''
+    },
+    queue: {
+        accountId: '',
+        accessId: '',
+        secretKey: '',
+        queues: {
+            common: '',
+            pay: ''
+        }
+    },
+    database: {
+        host: '',
+        port: '',
+        username: '',
+        password: '',
+        db: ''
+    }
+};
