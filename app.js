5cd939ff273bb69bd1f1e58d0f87f1076e582e90
bugall <769088641@qq.com>
2018-10-18 14:11:55 +0800

    working

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 'use strict';
+const config = require('./config');
+
 const Reward = require('./lib/reward');
 const Pay = require('./lib/pay');
-const config = require('./config');
+const Confirm = require('./lib/confirm');
+
 const reward = new Reward(config);
 const pay = new Pay(config);
+const confirm = new Confirm(config);
+
 const dbConn = require('./database/conn');
 
 // pre-start, check service state
 dbConn.authenticate().then(() => {
-    console.log('database  success');
+    console.log('Reward Service Start');
     // start
     return reward.start();
 }).catch((err) => {
     console.log(err, 'reward');
 });
+// pre-start, check service state
+dbConn.authenticate().then(() => {
+    console.log('Confirm Service Start');
+    // start
+    return confirm.start();
+}).catch((err) => {
+    console.log(err, 'pay');
+});
 
 // pre-start, check service state
 dbConn.authenticate().then(() => {
-    console.log('database  success');
+    console.log('Pay Service Start');
     // start
     return pay.start();
 }).catch((err) => {
